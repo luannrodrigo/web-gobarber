c894f757ca3d4401b2c5079f26273cd5585644de
Luann Rodrigo <luann.rodrigos@gmail.com>
2020-10-20 13:35:56 -0300

    Isolando componentes input e button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => (
   <Container>
@@ -10,9 +12,9 @@ const SignIn: React.FC = () => (
       <img src={logoImg} alt="Logo" />
       <form>
         <h1>Faça seu logon</h1>
-        <input type="text" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+        <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
+        <Input name="passoword" icon={FiLock} type="password" placeholder="Senha" />
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -26,45 +26,12 @@ export const Content = styled.div`
       margin-bottom: 24px;
     }
 
-    input {
-      background: #232129;
-      color: #f4ede8;
-      border-radius: 10px;
-      border: 2px solid #232129;
-      padding: 16px;
-      width: 100%;
-
-      &::placeholder {
-        color: #666360
-      }
-
-      & + input {
-        margin-top: 8px;
-      }
-    }
-
-    button {
-      background: #ff9000;
-      height: 56px;
-      border-radius: 10px;
-      border: 0;
-      padding: 16px;
-      color: #312e38;
-      width: 100%;
-      margin-top: 16px;
-      font-weight: 500;
-      transition: background-color 0.2s;
-
-      &:hover {
-        background: ${shade(0.2, '#ff9000')}
-      }
-    }
-
     a {
       color: #f4ede8;
       display: block;
       margin-top: 26px;
       transition: color 0.2s;
+      text-decoration: none;
 
       &:hover {
         color: ${shade(0.2, '#f4ede8')}
@@ -78,6 +45,7 @@ export const Content = styled.div`
     display: block;
     margin-top: 26px;
     transition: color 0.2s;
+    text-decoration: none;
 
     display: flex;
     align-items: center;
