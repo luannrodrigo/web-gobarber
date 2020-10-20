e9f423d6d7ca23c810bde291006bc3a5038f0db8
Luann Rodrigo <luann.rodrigos@gmail.com>
2020-10-20 12:07:20 -0300

    Criando estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => <h1>Hello Word</h1>;
-
+const App: React.FC = () => (
+  <>
+    <h1>Hello Word</h1>
+    <GlobalStyle />
+  </>
+);
 export default App;
