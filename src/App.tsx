92b96a3321ef9613a5feed0cab910fda82e93ea2
Luann Rodrigo <luann.rodrigos@gmail.com>
2020-10-21 13:49:35 -0300

    Context Api

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,15 @@ import SingIn from './pages/SignIn';
 // import SingUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
+import AuthContex from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SingIn />
-    {/* <SingUp /> */}
+    <AuthContex.Provider value={{ name: 'Luann' }}>
+
+      <SingIn />
+      {/* <SingUp /> */}
+    </AuthContex.Provider>
     <GlobalStyle />
   </>
 );
