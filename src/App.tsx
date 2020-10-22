a3fde50b70a2d65b0c9241128cd5c13373204ccb
Luann Rodrigo <luann.rodrigos@gmail.com>
2020-10-22 08:54:09 -0300

    Criando estrutura de toast

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import SingIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
 
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
@@ -10,6 +11,8 @@ const App: React.FC = () => (
     <AuthProvider>
       <SingIn />
     </AuthProvider>
+
+    <ToastContainer />
 
     <GlobalStyle />
   </>
