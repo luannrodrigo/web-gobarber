f6736fe0b732979686df7bcdc00f923a011a82c8
Luann Rodrigo <luann.rodrigos@gmail.com>
2020-11-19 14:46:00 -0300

    Criando section proximo atendimento

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 import {
-  Container, Header, HeaderContent, Profile, Content, Schedule, Calendar,
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
 } from './styles';
 
 const Dashboard: React.FC = () => {
   const { singOut, user } = useAuth();
-
-  console.log(user);
 
   return (
     <Container>
@@ -35,8 +40,30 @@ const Dashboard: React.FC = () => {
 
       <Content>
         <Schedule>
-          <Calendar />
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 20</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars2.githubusercontent.com/u/15133324?s=400&u=c4c6f588f322d6994f1c318cfea66d05267b7a2f&v=4"
+                alt="Luann Rodrigo"
+              />
+
+              <strong>Luann Rodrigo</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
         </Schedule>
+        <Calendar />
       </Content>
     </Container>
   );
